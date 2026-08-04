b6f13c55a6aac35e825a25ffb7190ec2c3432e76
egorywka <egorbeckish@mail.ru>
2026-08-04 21:37:18 +0700

    Add files via upload

---
+++ SQL+PL-pgSQL/SQL/TypesAndFunctions/5.DataTypeConversionFunctions/IntToStr/sql/query3.sql
@@ -0,0 +1,11 @@
+SET LC_MONETARY TO 'de-DE'; -- символ валюты €
+
+--|lc_monetary|
+--|-----------|
+--|de-DE      |
+
+SET LC_MONETARY TO 'en-US'; -- символ валюты $
+
+--|lc_monetary|
+--|-----------|
+--|en-US      |

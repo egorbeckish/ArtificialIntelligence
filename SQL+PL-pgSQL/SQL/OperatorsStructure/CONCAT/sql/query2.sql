c74042c6d8e5e243ba5441fbfac963466dd27b6e
egorbeckish <egorbeckish@mail.ru>
2026-09-04 14:09:51 +0400

    concat

---
+++ SQL+PL-pgSQL/SQL/OperatorsStructure/CONCAT/sql/query2.sql
@@ -0,0 +1,10 @@
+SELECT
+	CONCAT('Order ', order_id, ' from ', order_date, ' is ', status) AS Order_Status
+FROM
+	orders
+WHERE
+	salesman_id = 165;
+
+--|order_status                       |
+--|-----------------------------------|
+--|Order 66 from 2020-01-23 is Pending|

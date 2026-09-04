d948811ff0208d73da8bc5fe85c93744977bd984
egorbeckish <egorbeckish@mail.ru>
2026-09-04 15:20:14 +0400

    lpad

---
+++ SQL+PL-pgSQL/SQL/TypesAndFunctions/CharacterType/LPAD/sql/query2.sql
@@ -0,0 +1,15 @@
+SELECT
+	LPAD(first_name || ' ' || last_name, 20) || ' has a monthly salary of ' || RPAD(TO_CHAR(salary, '999999D99'), 9) || ' dollars.' AS Pay
+FROM
+	employees
+WHERE
+	salary > 12000;
+
+--|pay                                                            |
+--|---------------------------------------------------------------|
+--|         Steven King has a monthly salary of   24000,0 dollars.|
+--|       Neena Kochhar has a monthly salary of   17000,0 dollars.|
+--|         Lex De Haan has a monthly salary of   17000,0 dollars.|
+--|        John Russell has a monthly salary of   14000,0 dollars.|
+--|      Karen Partners has a monthly salary of   13500,0 dollars.|
+--|   Michael Hartstein has a monthly salary of   13000,0 dollars.|
